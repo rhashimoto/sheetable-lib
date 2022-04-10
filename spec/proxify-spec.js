34f18af570efad9ab3c75f3cc760e2fcead90620
Roy Hashimoto <roy@shoestringresearch.com>
2022-04-10 11:56:36 -0700

    Add source and tests.

---
+++ spec/proxify-spec.js
@@ -0,0 +1,137 @@
+import { proxify, transfer } from '../dist/proxify.js';
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 30 * 1000;
+
+describe('proxify', function() {
+  let port1, port2;
+  beforeEach(function() {
+    ({port1, port2} = new MessageChannel());
+  });
+
+  afterEach(function() {
+    port1.close();
+    port2.close();
+  });
+
+  it('should call a function target', async function() {
+    function target(a, b) {
+      return a + b;
+    }
+    proxify(port1, target);
+    const proxy = proxify(port2);
+
+    const result = proxy(1, 2);
+    await expectAsync(result).toBeResolvedTo(3);
+  });
+
+  it('should call a target member function', async function() {
+    const target = {
+      add(a, b) {
+        return a + b;
+      }
+    };
+    proxify(port1, target);
+    const proxy = proxify(port2);
+
+    const result = proxy.add(1, 2);
+    await expectAsync(result).toBeResolvedTo(3);
+  });
+
+  it('should propagate Error', async function() {
+    function target() {
+      throw new Error('foo');
+    }
+    proxify(port1, target);
+    const proxy = proxify(port2);
+
+    const result = proxy();
+    await expectAsync(result).toBeRejectedWithError(/foo/);
+  });
+
+  it('should throw after port is closed', async function() {
+    const target = proxify(port1);
+    port1.close();
+
+    const proxy = proxify(port2);
+
+    // Wait for the close to propagate.
+    await new Promise(resolve => {
+      const close = port2.close;
+      port2.close = function() {
+        close.apply(port2);
+        resolve();
+      };
+    });
+
+    const result = proxy();
+    await expectAsync(result).toBeRejectedWithError(/closed/);
+  });
+
+  it('should transfer argument', async function() {
+    function target(arrayBuffer) {
+      return arrayBuffer.byteLength;
+    }
+    proxify(port1, target);
+    const proxy = proxify(port2);
+
+    const result = proxy(new ArrayBuffer(42));
+    await expectAsync(result).toBeResolvedTo(42);
+  });
+
+  it('should transfer result', async function() {
+    function target() {
+      return new ArrayBuffer(42);
+    }
+    proxify(port1, target);
+    const proxy = proxify(port2);
+
+    const result = await proxy();
+    await expect(result.byteLength).toBe(42);
+  });
+
+  it('should transfer complex argument', async function() {
+    function target(buffers) {
+      return buffers.map(buffer => buffer.byteLength);
+    }
+    proxify(port1, target);
+    const proxy = proxify(port2);
+
+    const buffers = [new ArrayBuffer(1), new ArrayBuffer(2), new ArrayBuffer(3)];
+    const result = proxy(transfer(buffers, buffers));
+    await expectAsync(result).toBeResolvedTo([1, 2, 3]);
+  });
+
+  it('should transfer complex result', async function() {
+    function target() {
+      const buffers = [new ArrayBuffer(1), new ArrayBuffer(2), new ArrayBuffer(3)];
+      return transfer(buffers, buffers);
+    }
+    proxify(port1, target);
+    const proxy = proxify(port2);
+
+    const result = await proxy();
+    await expect(result.map(buffer => buffer.byteLength)).toEqual([1, 2, 3]);
+  });
+
+  it('should auto close', async function() {
+    const target = new Promise(resolve => {
+      const close = port1.close;
+      port1.close = function() {
+        close.apply(this);
+        resolve();
+      }
+      proxify(port1, {});
+    });
+    const proxy = new Promise(resolve => {
+      const close = port2.close;
+      port2.close = function() {
+        close.apply(this);
+        resolve();
+      }
+      proxify(port2);
+    });
+
+    await expectAsync(target).toBeResolved();
+    await expectAsync(proxy).toBeResolved();
+  });
+});
---
+++ src/proxify.ts
@@ -0,0 +1,140 @@
+const TRANSFERABLES = new Set([MessagePort, ArrayBuffer]);
+const UNREACHABLE = Symbol();
+
+interface MessagePortLike {
+  postMessage: (data: any, transferables?: Transferable[]) => void
+  addEventListener: (type: "message", listener: (event: MessageEvent<any>) => void) => void
+  removeEventListener: (type: "message", listener: (event: MessageEvent<any>) => void) => void
+
+  start?: () => void
+  close?: () => void
+}
+
+type PromiseCallbacks = { resolve: (value: unknown) => void, reject: (reason?: any) => void };
+
+const mapPortToPromiseCallbacks = new WeakMap<MessagePortLike, Map<string, PromiseCallbacks>>();
+const mapObjectToTransferables = new WeakMap<any, Transferable[]>();
+
+// Close port when proxy is garbage collected.
+const registry = new FinalizationRegistry(function(port: MessagePortLike) {
+  port.close?.();
+});
+
+export function proxify(port: MessagePortLike, target?: Function|object) {
+  port.start?.();
+  const close = port.close;
+  if (target) {
+    async function listener({ data }) {
+      if (data.close) {
+        port.close();
+        return;
+      }
+
+      const id = data.id;
+      try {
+        // Dereference the member path.
+        const [obj, member] = data.path.reduce(([_, obj], property) => {
+          return [obj, obj[property]];
+        }, [null, target]);
+
+        const result = await member.apply(obj, data.args);
+        const transferables = new Set([
+          TRANSFERABLES.has(result?.constructor) ? result : [],
+          mapObjectToTransferables.get(result) ?? []
+        ].flat());
+        port.postMessage({ id, result }, [...transferables]);
+      } catch (e) {
+        // Error is not structured cloneable on all platforms.
+        const error = e instanceof Error ? {
+          message: e.message,
+          stack: e.stack
+        } : e;
+        port.postMessage({ id, error });
+      }
+    }
+    port.addEventListener('message', listener);
+
+    // When the port is closed, notify the other endpoint and remove
+    // the listener so the target is no longer referenced.
+    port.close = function() {
+      port.postMessage({ close: true });
+      close?.apply(port);
+      port.removeEventListener('message', listener);
+    };
+  } else {
+    // Create map to match a response with Promise callbacks.
+    const callbacks = new Map();
+    mapPortToPromiseCallbacks.set(port, callbacks);
+    function listener({ data }) {
+      if (data.close) {
+        port.close();
+        return;
+      }
+
+      const callback = callbacks.get(data.id);
+      if (data.hasOwnProperty('result')) {
+        callback.resolve(data.result);
+      } else {
+        callback.reject(Object.assign(new Error(), data.error));
+      }
+      callbacks.delete(data.id);
+    }
+    port.addEventListener('message', listener);
+
+    // Override the close method to notify the other endpoint.
+    port.close = function() {
+      port.postMessage({ close: true });
+      close?.apply(port);
+      port.removeEventListener('message', listener);
+
+      // Reject any outstanding calls.
+      const callbacks = mapPortToPromiseCallbacks.get(port);
+      if (callbacks) {
+        for (const callback of callbacks.values()) {
+          callback.reject(new Error('port closed'));
+        }
+        mapPortToPromiseCallbacks.delete(port);
+      }
+    };
+
+    const proxy = makeProxy(port, null, []);
+    registry.register(proxy, port);
+    return proxy;
+  }
+}
+
+function makeProxy(port: MessagePortLike, parentProxy: any, path: (string|symbol)[]) {
+  const proxy = new Proxy(function(){}, {
+    get(_, property, receiver) {
+      // The only reason for this is to prevent garbage collection of
+      // the root proxy while any related proxies are in use.
+      if (property === UNREACHABLE) return parentProxy;
+
+      // Avoid confusing a proxy with a Promise, e.g. on return from
+      // an async function.
+      if (property === 'then') return undefined;
+
+      return makeProxy(port, receiver, [...path, property]);
+    },
+
+    apply(_, __, args) {
+      const callbacks = mapPortToPromiseCallbacks.get(port);
+      if (!callbacks) return Promise.reject(new Error('port closed'));
+      return new Promise(function(resolve, reject) {
+        const id = Math.random().toString(36).replace('0.', '');
+        callbacks.set(id, { resolve, reject });
+        const transferables = new Set([
+          args.filter(arg => TRANSFERABLES.has(arg?.constructor)),
+          args.map(arg => mapObjectToTransferables.get(arg) ?? [])
+        ].flat(Infinity));
+        port.postMessage({ id, path, args }, [...transferables]);
+      });
+    }
+  });
+  return proxy;
+}
+
+ export function transfer(obj: any, transferables: Transferable[]) {
+  mapObjectToTransferables.set(obj, transferables);
+  return obj;
+}
